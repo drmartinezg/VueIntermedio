d3a36eb0861aa24e36a9068dab340d2576f1eaa5
drmartinezg <drmartinezg2@yahoo.es>
2021-01-01 13:06:58 +0100

    Enrutador personalizado

---
+++ 10-Custom_Vue_Router/README.md
@@ -0,0 +1,3 @@
+# Enrutador personalizado
+
+Como paso previo para entender cómo funciona y para qué sirve [Vue Router](https://router.vuejs.org/), vamos a crear un pequeño enrutador de forma _artesanal_ y así poner en práctica conceptos esenciales sobre Vue, componentes dinámicos y rutas.
---
+++ 10-Custom_Vue_Router/custom-vue-router/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+      <!-- <BlackCat imagen="cat1.jpg"/> 
+      <BlackCat imagen="cat2.jpg"/> 
+      <BlackCat imagen="cat3.jpg"/>  -->
+      <ul>
+        <RouterLink destino="black-cat-1" />
+        <RouterLink destino="black-cat-2" />
+        <RouterLink destino="black-cat-3" />
+      </ul>
+      <RouterView/>
+  </div>
+</template>
+
+<script>
+import RouterView from './components/RouterView.vue';
+import RouterLink from './components/RouterLink.vue';
+export default {
+  name: 'app',
+  components: {
+    RouterView,
+    RouterLink,
+  }
+}
+</script>
+
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    padding: 0;
+    position: absolute;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
